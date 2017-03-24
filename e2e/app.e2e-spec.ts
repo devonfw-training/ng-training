3aa9ea9617c8902cd066d635bd5fb3a6652f5496
Angular CLI <angular-cli@angular.io>
2017-03-24 14:36:13 +0100

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { CliTestNg4Page } from './app.po';
+
+describe('cli-test-ng4 App', () => {
+  let page: CliTestNg4Page;
+
+  beforeEach(() => {
+    page = new CliTestNg4Page();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
