a8453a15390bf05ea15827c727d0827f4cac7bc6
mmatczak <marek.matczak@capgemini.com>
2017-03-25 20:00:11 +0100

    Hello World added

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('cli-test-ng4 App', () => {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Hello World!');
   });
 });
