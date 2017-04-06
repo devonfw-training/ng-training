c965e8b485e9c6e6fb8c3ce81717aec93a6d2533
mmatczak <marek.matczak@capgemini.com>
2017-04-06 23:10:10 +0200

    Template-driven forms implemented

--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {Book} from '../book';
 import {cloneDeep} from 'lodash';
 import {BookService} from '../book.service';
 import {Router, ActivatedRoute} from '@angular/router';
+import {NgForm, AbstractControl} from '@angular/forms';
 
 @Component({
   selector: 'app-book-details',
@@ -12,8 +13,43 @@ import {Router, ActivatedRoute} from '@angular/router';
 export class BookDetailsComponent implements OnInit {
   book: Book;
 
+  @ViewChild('bookForm')
+  currentForm: NgForm;
+
+  submitted: boolean;
+
+  private static createErrorMessage(errorObject: {[key: string]: any}): string {
+    if (errorObject) {
+      for (const errorCode in errorObject) {
+        if (errorObject.hasOwnProperty(errorCode)) {
+          switch (errorCode) {
+            case 'required':
+              return 'Please provide a value';
+            case 'maxlength':
+              return 'The value is too long';
+            default:
+              return 'The value is wrong';
+          }
+        }
+      }
+    }
+  };
+
   constructor(private bookService: BookService, private router: Router, private route: ActivatedRoute) {
     this.book = new Book();
+    this.submitted = false;
+  }
+
+  getErrorMessageOfField(fieldName: string): string {
+    if (this.isFieldInvalid(fieldName)) {
+      const fieldControl: AbstractControl = this.currentForm.form.get(fieldName);
+      return BookDetailsComponent.createErrorMessage(fieldControl.errors);
+    }
+  }
+
+  isFieldInvalid(fieldName: string): boolean {
+    const fieldControl: AbstractControl = this.currentForm.form.get(fieldName);
+    return fieldControl && fieldControl.invalid && (fieldControl.touched || this.submitted);
   }
 
   ngOnInit(): void {
@@ -25,8 +61,11 @@ export class BookDetailsComponent implements OnInit {
   }
 
   apply(): void {
-    this.bookService.save(this.book).subscribe(() => {
-      this.router.navigate(['/book-app/books']);
-    });
+    this.submitted = true;
+    if (this.currentForm.form.valid) {
+      this.bookService.save(this.book).subscribe(() => {
+        this.router.navigate(['/book-app/books']);
+      });
+    }
   }
 }
