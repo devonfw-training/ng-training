c418443ab43e37a9c642e33d66228328905eb9b3
Szewcow Tomasz <tomasz.szewcow.ext@adcubum.com>
2017-09-21 13:33:37 +0200

    Tests for book service findOne and save added

--- src/app/books/book.service.spec.ts
+++ src/app/books/book.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, inject, async } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { BookService } from './book.service';
 import { Book } from './book';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
@@ -17,8 +17,6 @@ describe('BookService', () => {
     httpMock = TestBed.get(HttpTestingController);
     bookService = TestBed.get(BookService);
   });
-
-
 
   it('should find all books', () => {
     // given
@@ -47,7 +45,42 @@ describe('BookService', () => {
     });
 
     httpMock.expectOne('services/rest/books').flush(mockResponse);
-
-    httpMock.verify();
   });
+
+  it('should find single book', () => {
+    // given
+    const bookId = 1;
+    const mockResponse = {
+      id: bookId,
+      title: 'title1',
+      authors: 'authors1'
+    };
+
+    //when
+    bookService.findOne(bookId).subscribe(book => {
+      // then
+      expect(book).toBeDefined();
+      expect(book.id).toBe(bookId);
+      expect(book.title).toBe(mockResponse.title);
+      expect(book.authors).toBe(mockResponse.authors);
+    });
+
+    httpMock.expectOne(`services/rest/book/${bookId}`);
+  });
+
+  it('should save book', () => {
+    // given
+    const bookToSave = Book.from('authors', 'title');
+
+    // when
+    bookService.save(bookToSave).subscribe(book => {
+      // then
+      expect(book.authors).toBe(bookToSave.authors);
+      expect(book.title).toBe(bookToSave.title);
+    });
+
+    httpMock.expectOne('services/rest/book');
+  });
+
+  afterEach(() => httpMock.verify());
 });
