ca2a497ce232bbccc244d6af8ef22f33d68e8826
Szewcow Tomasz <tomasz.szewcow.ext@adcubum.com>
2018-02-26 12:04:37 +0100

    forms bootstrap 4 migration; rxjs pipeable operators

--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -1,9 +1,9 @@
-import {Injectable} from '@angular/core';
-import {Book} from './book';
-import {Observable} from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import {assign, cloneDeep} from 'lodash';
-import {HttpClient} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Book } from './book';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
+import { assign, cloneDeep } from 'lodash';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class BookService {
@@ -12,19 +12,22 @@ export class BookService {
   }
 
   findAll(): Observable<Book[]> {
-    return this.http.get('services/rest/books')
-      .map((booksFromServer: BookFromBackend[]) => booksFromServer.map(this.fromBackend))
-      .map((books: Book[]) => cloneDeep(books));
+    return this.http.get('services/rest/books').pipe(
+      map((booksFromServer: BookFromBackend[]) => booksFromServer.map(this.fromBackend)),
+      map((books: Book[]) => cloneDeep(books))
+    );
   }
 
   findOne(id: number): Observable<Book> {
-    return this.http.get('services/rest/book/' + id)
-      .map(this.fromBackend);
+    return this.http.get('services/rest/book/' + id).pipe(
+      map(this.fromBackend)
+    )
   }
 
   save(bookToSave: Book): Observable<Book> {
-    return this.http.post('services/rest/book', this.toBackend(bookToSave))
-      .map(this.fromBackend);
+    return this.http.post('services/rest/book', this.toBackend(bookToSave)).pipe(
+      map(this.fromBackend)
+    );
   }
 
   toBackend(book: Book): BookFromBackend {
