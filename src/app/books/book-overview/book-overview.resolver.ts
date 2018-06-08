1942e60e051dd5d73b7fcc3fdca86586b6ba3bdd
mmatczak <marek.matczak@capgemini.com>
2018-06-08 12:29:35 +0200

    Migration to Angular 6 / RxJS 6

--- src/app/books/book-overview/book-overview.resolver.ts
+++ src/app/books/book-overview/book-overview.resolver.ts
@@ -1,10 +1,12 @@
 import {Resolve} from '@angular/router';
 import {Book} from '../book';
-import {Observable} from 'rxjs/Observable';
+import {Observable} from 'rxjs';
 import {Injectable} from '@angular/core';
 import {BookService} from '../book.service';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class BookOverviewResolver implements Resolve<Book[]> {
   constructor(private bookService: BookService) {
   }
